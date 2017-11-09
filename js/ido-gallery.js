b5f2ff4235e0cdf2a1080e415340e69bc8ef24e0
ido tirem <idotirem@gmail.com>
2017-11-09 12:27:21 +0200

    myfirst commit!

---
+++ js/ido-gallery.js
@@ -0,0 +1,46 @@
+'use srict'
+console.log('ido-gallery');
+
+var gProjs = [
+    {
+        id: "sokoban",
+        name: "Sokoban",
+        title: "Better push those boxes",
+        desc: "lorem ipsum lorem ipsum lorem ipsum1",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "chess",
+        name: "chess",
+        title: "kill the king",
+        desc: "lorem ipsum lorem ipsum lorem ipsum2",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "culc",
+        name: "culc",
+        title: "lats do math",
+        desc: "lorem ipsum lorem ipsum lorem ipsum3",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "buloons",
+        name: "buloons",
+        title: "pop pop pop",
+        desc: "lorem ipsum lorem ipsum lorem ipsum4",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    }
+]
+
+function initPage() {
+    console.log('initPage function');
+    //set the progs
+    for (var i = 0; i < gProjs.length; i++) {
+        
+    }
+}
+
